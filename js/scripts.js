d1634836f276cf0849802f63b69cb1c219f65301
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-02-01 18:21:42 +0200

    make hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,38 @@ const swiper = new Swiper('.swiper-container', {
     //   el: '.swiper-scrollbar',
     // },
   });
+
+let map;
+
+function initMap() {
+    const myLatLng = { 
+        lat: 47.1286361736067, 
+        lng: 37.55897540422409 
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: myLatLng,
+    });
+    var iconBase = 'img/';
+    var marker = new google.maps.Marker({
+        title: "Mari I Poll",
+        position: myLatLng,
+        map: map,
+        icon: iconBase + 'marker-gmaps.png'
+    });
+}
+
+document.querySelector('.humburger').addEventListener('click', function(e) {
+    e.preventDefault();
+    // this.classList.toggle('is-active');
+    if (this.classList.contains('is-active')) {
+        this.classList.remove('is-active');
+        document.querySelector('#menu').classList.remove('nav-active');
+        document.querySelector('.mip__hamburger').classList.remove('body-active');
+    }
+    else {
+        this.classList.add('is-active');
+        document.querySelector('#menu').classList.add('nav-active');
+        document.querySelector('.mip__hamburger').classList.add('body-active');
+    }
+})
